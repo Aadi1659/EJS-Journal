60b078bf6b1997a6e7141799b46bf748be7e1454
Aadi1659 <aadityadev1659@gmail.com>
2022-09-11 18:24:38 +0530

    V 0.0.2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const _ = require('lodash');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -11,6 +11,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 let posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,7 +20,39 @@ app.use(express.static("public"));
 app.get("/",function(req,res){
     res.render("home",{
       homeStartingContent : homeStartingContent,
+      posts:posts,
+      
+      
     });
+})
+
+
+// suppose we have a number of routes for out individual blogs like posts/day 1 posts/day 2 etc etc.
+// for that what we can do is use the route parameters which takes the input in the route and uses that as a variable.
+
+// app.get('/users/:userId/books/:bookId', (req, res) => {
+//   // Access userId via: req.params.userId
+//   // Access bookId via: req.params.bookId
+//   res.send(req.params);
+// })
+app.get("/posts/:postName",function(req,res){
+  const requestedTitle = _.lowerCase(req.params.postName)
+  posts.forEach(function(postName){
+    if(requestedTitle===_.lowerCase(postName.title)){
+      console.log("Match Found!");
+      res.render("post",{
+        requestedTitle: postName.title,
+        blogContent: postName.post,
+        
+      })
+      
+    }
+    else{
+      console.log("Not found!");
+    }
+  })
+  
+  
 })
 
 
@@ -45,16 +78,19 @@ app.post("/compose",function(req,res){
     post : req.body.post,
   }
   posts.push(post);
-  console.log(posts);
-
-  
-
-  res.redirect("/")
+  res.redirect("/");
 })
-
-
-
-
+  
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+ 
+  
+    
+
+
+
+
+
+
+
