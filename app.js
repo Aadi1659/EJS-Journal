379aa25e932042876932defa01b6a85d61b1c41f
Aadi1659 <aadityadev1659@gmail.com>
2022-10-05 17:18:36 +0530

    Added Heroku modification
    App is all ready to fire up

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -17,13 +18,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://admin-aadi:Test-1659@cluster0.l8yhchl.mongodb.net/blog-website");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Blog = mongoose.model("Blog",blogSchema);
+
 app.get("/",function(req,res){
-    res.render("home",{
-      homeStartingContent : homeStartingContent,
-      posts:posts,
-      
-      
-    });
+    
+    Blog.find({},function(err,blogs){
+      res.render("home",{
+        homeStartingContent : homeStartingContent,
+        posts:blogs,
+           
+      });
+    })
+    
 })
 
 
@@ -36,24 +49,23 @@ app.get("/",function(req,res){
 //   res.send(req.params);
 // })
 app.get("/posts/:postName",function(req,res){
-  const requestedTitle = _.lowerCase(req.params.postName)
-  posts.forEach(function(postName){
-    if(requestedTitle===_.lowerCase(postName.title)){
-      console.log("Match Found!");
+  const requestedId = req.params.postName;
+  Blog.findOne({_id:requestedId},function(err,blog){
+    if(!err){
+      console.log("Rendering "+ requestedId);
       res.render("post",{
-        requestedTitle: postName.title,
-        blogContent: postName.post,
-        
+        requestedTitle:blog.title,
+        blogContent:blog.content,
       })
-      
     }
+  
     else{
-      console.log("Not found!");
+        console.log("Not found");
     }
   })
-  
-  
 })
+
+  
 
 
 app.get("/about",function(req,res){
@@ -77,11 +89,22 @@ app.post("/compose",function(req,res){
     title : req.body.title,
     post : req.body.post,
   }
-  posts.push(post);
-  res.redirect("/");
+  const blog = new Blog({
+    title:req.body.title,
+    content:req.body.post,
+  })
+  blog.save(function(err){
+    if(!err){
+      posts.push(post);
+      res.redirect("/");
+    }
+
+  });
 })
   
-app.listen(3000, function() {
+let port = process.env.PORT;
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
  
